bb65f252963de0627aa62fb9429fc5bb5b9f381f
laxamore <mukhliskurnia60@gmail.com>
2019-06-06 12:47:38 +0700

    Update Robot Field

--- src/rostuUi.py
+++ src/rostuUi.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python
 
+from __future__ import division
+
+import numpy as np
+import cv2
+import imutils
+import sys
+import roslib
+import rospy
 import rospkg
 
+from pubsub import pub
+from geometry_msgs.msg import PoseStamped 
+from geometry_msgs.msg import PoseWithCovarianceStamped 
+from geometry_msgs.msg import Pose 
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
@@ -16,11 +28,15 @@ class Worker(QRunnable):
         result = self.fn()
 
 class rostu(QWidget):
-    path = rospkg.RosPack().get_path('rostu_gui')
-
     def __init__(self):
         super(rostu, self).__init__()
+        self.path = rospkg.RosPack().get_path('rostu_gui')
+        
+        self.robotPose = Pose()
+
+        self.initSub()
         self.initUI()
+
         self.setMouseTracking(True)
         self.fieldHover = False
 
@@ -32,30 +48,40 @@ class rostu(QWidget):
         
         self.threadpool = QThreadPool()
         print("Multithreading with maximum %d threads" % self.threadpool.maxThreadCount())
-        
-    def updateField(self):
-        self.image = cv2.imread(self.path + '/media/field.png')
-        height, width, byteValue = self.image.shape
-        byteValue = byteValue * width
-
-        cv2.cvtColor(self.image, cv2.COLOR_BGR2RGB, self.image)
-        self.mQImage = QImage(self.image, width, height, byteValue, QImage.Format_RGB888)
-
-        pixmap = QPixmap(self.mQImage)
-        pixmap = pixmap.scaled(pixmap.width() / 7, pixmap.height() / 7)
-        self.field.setPixmap(pixmap)
-        self.field.resize(pixmap.width(), pixmap.height())
-        self.field.installEventFilter(self)
-
-    def updateFieldThread(self):
-        worker = Worker(self.updateField)
-        self.threadpool.start(worker) 
 
     def initUI(self):
         #Load Design UI Dari QtDesign
         loadUi(self.path + '/qt_ui/rostuUi.ui', self)
         #Setting Fixed Frame Window
         self.setFixedSize(1280, 680)
+
+    def initSub(self):
+        self.s_p_sub = rospy.Subscriber('/amcl_pose', PoseWithCovarianceStamped, self.striker_pose_callback)
+
+    def striker_pose_callback(self, data):
+        self.robotPose = data.pose.pose
+        print(self.robotPose)
+
+    def updateField(self):
+        self.image = cv2.imread(self.path + '/media/field.png')
+        height, width = self.image.shape[:2]
+        
+        self.image = cv2.resize(self.image, (int(width / 7), int(height / 7)))
+        self.image = imutils.rotate_bound(self.image, 90)
+
+        height, width, byteValue = self.image.shape
+        byteValue = byteValue * width
+        cv2.cvtColor(self.image, cv2.COLOR_BGR2RGB, self.image)
+
+        self.mQImage = QImage(self.image, width, height, byteValue, QImage.Format_RGB888)
+        self.field.setPixmap(QPixmap(self.mQImage))
+        self.field.resize(width, height)
+        self.field.installEventFilter(self)
+        
+
+    def updateFieldThread(self):
+        worker = Worker(self.updateField)
+        self.threadpool.start(worker) 
 
     def eventFilter(self, object, event):
         if event.type() == QEvent.Enter:
@@ -93,25 +119,16 @@ class rostu(QWidget):
         goal_publish_msg.pose.orientation.z = -0.711607695391
         goal_publish_msg.pose.orientation.w = 0.702577033399
         publish_goal.publish(goal_publish_msg)
-        # rate.sleep()
 
-
-        
 if __name__ == "__main__":
-    import cv2
-    import sys
-    import rospy
-
-    from geometry_msgs.msg import PoseStamped 
-
-    rospy.init_node('rostu_gui')
-    publish_goal = rospy.Publisher('move_base_simple/goal', PoseStamped, queue_size=1)
-    # rate = rospy.Rate(30)
-
     app = QApplication(sys.argv)
     window = rostu()
     window.setWindowTitle('ROSTU')
     window.show()
+
+    rospy.init_node('rostu_gui')
+    publish_goal = rospy.Publisher('move_base_simple/goal', PoseStamped, queue_size=1)    
+
     sys.exit(app.exec_())
     
 
